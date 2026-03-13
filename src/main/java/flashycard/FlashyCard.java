fe710d371572dae5f77090de478658029befcc05
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-03-13 15:08:11 +0800

    Fix command

--- src/main/java/flashycard/FlashyCard.java
+++ src/main/java/flashycard/FlashyCard.java
@@ -29,7 +29,7 @@ public class FlashyCard {
             if (input == null) {
                 continue;
             }
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("exit")) {
                 break;
             }
             System.out.println("Command received: " + input);
