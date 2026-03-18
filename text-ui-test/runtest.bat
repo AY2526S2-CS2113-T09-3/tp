c85eef7d52e638f5909a4390ecb90e5e54e823b1
cyanduck32 <msproton858@gmail.com>
2026-03-19 00:07:20 +0800

    fix runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,27 +6,14 @@ cd ..
 call gradlew clean shadowJar
 
 cd build\libs
-set jarloc=
-for /f "tokens=*" %%a in ('dir /b *.jar') do (
+for /f "tokens=*" %%a in (
+    'dir /b *.jar'
+) do (
     set jarloc=%%a
 )
 
-java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT 2>&1
+java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
 
 cd ..\..\text-ui-test
 
-:waitForFile
-2>nul (>>ACTUAL.TXT (call )) && (goto :compare) || (
-    timeout /t 1 /nobreak >nul
-    goto :waitForFile
-)
-
-:compare
-FC /W ACTUAL.TXT EXPECTED.TXT >NUL && (
-    ECHO Test passed!
-) || (
-    ECHO Test failed!
-    exit /b 1
-)
-
-popd
+FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
