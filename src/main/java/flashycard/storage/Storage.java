1f271a76f800b98372239646b6d42f540dcff196
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-03-20 00:52:13 +0800

    fix check style

--- src/main/java/flashycard/storage/Storage.java
+++ src/main/java/flashycard/storage/Storage.java
@@ -4,7 +4,11 @@ import flashycard.exceptions.CorruptedDataException;
 import flashycard.model.Card;
 import flashycard.model.KnowledgeBase;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
